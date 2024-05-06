7a983462f00c1b0d7f465c5fa416ded3c37d99c8
Muhammed Abdelmukaram <muhammed.abdelmukaram@stu.ssst.edu.ba>
2024-05-06 12:40:53 +0200

    Update

--- src/app/page.js
+++ src/app/page.js
@@ -69,7 +69,7 @@ export default function Home() {
 
   const handleImageLoad = (event) => {
     console.log(event.target.complete);
-    setIsLoadedImg(false);
+    setIsLoadedImg(true);
   };
 
   return (
