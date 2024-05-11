e64db15b042a8911a1ffbe07791c0227208dfdd2
Muhammed Abdelmukaram <muhammed.abdelmukaram@stu.ssst.edu.ba>
2024-05-11 11:22:35 +0200

    Update

--- src/app/page.js
+++ src/app/page.js
@@ -24,197 +24,196 @@ import FAQ from "@/app/sections/FAQ";
 import React, { useEffect, useRef, useState } from "react";
 
 {
-  /*<LoadingScreen/>*/
+    /*<LoadingScreen/>*/
 }
 
 export default function Home() {
-  const audioRef = useRef(null);
-  const [currentSongIndex, setCurrentSongIndex] = useState(0);
-  const [isPlaying, setIsPlaying] = useState(false);
-  const [isLoadedImg, setIsLoadedImg] = useState(false);
-  const songs = [
-    { src: "/GetPaidAnthemn.mp3", title: "Get Paid", artist: "Young Dolph" },
-    { src: "/StaySchemin.mp3", title: "Stay Schemin", artist: "Rick Ross" },
-  ];
+    const audioRef = useRef(null);
+    const [currentSongIndex, setCurrentSongIndex] = useState(0);
+    const [isPlaying, setIsPlaying] = useState(false);
+    const [isLoadedImg, setIsLoadedImg] = useState(false);
+    const songs = [
+        { src: "/GetPaidAnthemn.mp3", title: "Get Paid", artist: "Young Dolph" },
+        { src: "/StaySchemin.mp3", title: "Stay Schemin", artist: "Rick Ross" },
+    ];
 
-  useEffect(() => {
-    const script = document.createElement('script');
-    script.src = "https://tools.luckyorange.com/core/lo.js?site-id=97cedfd3";
-    script.async = true;
-    script.defer = true;
+    useEffect(() => {
+        const script = document.createElement('script');
+        script.src = "https://tools.luckyorange.com/core/lo.js?site-id=97cedfd3";
+        script.async = true;
+        script.defer = true;
 
-    document.body.appendChild(script);
+        document.body.appendChild(script);
 
-    return () => {
-      document.body.removeChild(script);
-    };
-  }, []);
+        return () => {
+            document.body.removeChild(script);
+        };
+    }, []);
 
-  const togglePlay = () => {
-    if (audioRef.current.paused) {
-      audioRef.current.play();
-      setIsPlaying(true);
-    } else {
-      audioRef.current.pause();
-      setIsPlaying(false);
-    }
-  };
-
-  const playNext = () => {
-    const nextIndex = (currentSongIndex + 1) % songs.length;
-    setCurrentSongIndex(nextIndex);
-    setIsPlaying(false);
-  };
-
-  const playPrevious = () => {
-    const prevIndex = (currentSongIndex - 1 + songs.length) % songs.length;
-    setCurrentSongIndex(prevIndex);
-    setIsPlaying(false);
-  };
-
-  // Ensure the audio source is updated when the song changes
-  if (audioRef.current) {
-    audioRef.current.src = songs[currentSongIndex].src;
-    if (isPlaying) {
-      audioRef.current.play();
-    }
-  }
-
-  const handleImageLoad = (event) => {
-    console.log(event.target.complete);
-    setIsLoadedImg(true);
-  };
-
-  return (
-    <>
-      {!isLoadedImg && <LoadingScreen />}
-      <main className={styles.main} style={{ opacity: !isLoadedImg ? 0 : 1 }}>
-
-
-        <div style={{ zIndex: 1 }}></div>
-        <div
-          style={{ width: "100vw", display: "flex", flexDirection: "column" }}
-        >
-          <AnnouncementBar />
-          <Header handleImageLoad={handleImageLoad} />
-          <Hero handleImageLoad={handleImageLoad} />
-        </div>
-
-        <div className={styles.hoverableDiv} onClick={togglePlay}></div>
-
-        {
-          <div
-            style={{
-              position: "fixed",
-              bottom: "10px",
-              right: "10px",
-              width: "330px",
-              height: "60px",
-              backgroundColor: "rgba(42, 42, 42, 0.8)",
-              borderRadius: "12px",
-              boxShadow: "0px 0px 8px rgba(0, 0, 0, 0.5)",
-              display: "flex",
-              alignItems: "center",
-              justifyContent: "flex-start", // Distribute space between icons
-              zIndex: 100,
-            }}
-          >
-            <Image
-              src="/SoundBars.png"
-              width={33}
-              height={33}
-              style={{
-                cursor: "pointer",
-                position: "absolute",
-                left: 20,
-                top: "50%",
-                transform: "translateY(-50%)",
-              }}
-              onLoad={handleImageLoad}
-            />
-
-            <div style={{ marginLeft: 68, width: 125 }}>
-              <p style={{ fontSize: 12 }}>{songs[currentSongIndex].artist}</p>
-              <p style={{ fontSize: 14 }}>{songs[currentSongIndex].title}</p>
-            </div>
-
-            <div
-              style={{
-                display: "flex",
-                flex: 1,
-                gap: 10,
-                marginLeft: 28,
-                alignItems: "center",
-              }}
-            >
-              <Image
-                src="/previous.png"
-                alt="Previous"
-                width={23}
-                height={23}
-                onClick={playPrevious}
-                style={{ cursor: "pointer" }}
-                onLoad={handleImageLoad}
-              />
-              <Image
-                src={isPlaying ? "/pause.png" : "/play.png"}
-                alt="Play/Pause"
-                width={23}
-                height={23}
-                onClick={togglePlay}
-                style={{ cursor: "pointer" }}
-                onLoad={handleImageLoad}
-              />
-              <Image
-                src="/next.png"
-                alt="Next"
-                width={23}
-                height={23}
-                onClick={playNext}
-                style={{ cursor: "pointer" }}
-                onLoad={handleImageLoad}
-              />
-            </div>
-          </div>
+    const togglePlay = () => {
+        if (audioRef.current.paused) {
+            audioRef.current.play();
+            setIsPlaying(true);
+        } else {
+            audioRef.current.pause();
+            setIsPlaying(false);
         }
+    };
 
-        <audio ref={audioRef} preload="auto"></audio>
+    const playNext = () => {
+        const nextIndex = (currentSongIndex + 1) % songs.length;
+        setCurrentSongIndex(nextIndex);
+        setIsPlaying(false);
+    };
 
-        <Benefits />
+    const playPrevious = () => {
+        const prevIndex = (currentSongIndex - 1 + songs.length) % songs.length;
+        setCurrentSongIndex(prevIndex);
+        setIsPlaying(false);
+    };
 
-        <div style={{ width: "100%", height: "100%", position: "relative" }}>
-          {/* Background image with adjusted positioning */}
-          {/*<div style={{
-                    position: 'absolute',
-                    top: "6364px",
-                    left: "424px",
-                    width: '100%',
-                    height: '57vh',
-                    zIndex: 1
-                }}>
-                    <Image
-                        src="/Astronaut.png" // Ensure the path is correct
-                        alt="Astronaut"
-                        layout="fill"
-                        objectFit="contain"
-                        priority
-                    />
-                </div>*/}
-          <div style={{ zIndex: 4 }}>
-            <Carousel />
-            <MemberCards />
-            <Reroll />
-          </div>
-          <div className={styles.gradientBackground}>
-            {/*<RoadmapNew/>*/}
-            <SeasonTwo />
-            <Team />
-            <Advisors />
-          </div>
-          <FAQ />
-          <Footer />
-        </div>
-      </main>
-    </>
-  );
+    // Ensure the audio source is updated when the song changes
+    if (audioRef.current) {
+        audioRef.current.src = songs[currentSongIndex].src;
+        if (isPlaying) {
+            audioRef.current.play();
+        }
+    }
+
+    const handleImageLoad = (event) => {
+        console.log(event.target.complete);
+        setIsLoadedImg(true);
+    };
+
+    return (
+        <>
+            {!isLoadedImg && <LoadingScreen />}
+            <main className={styles.main} style={{ opacity: !isLoadedImg ? 0 : 1 }}>
+                <div style={{ zIndex: 1 }}></div>
+                <div
+                    style={{ width: "100vw", display: "flex", flexDirection: "column" }}
+                >
+                    <AnnouncementBar />
+                    <Header handleImageLoad={handleImageLoad} />
+                    <Hero handleImageLoad={handleImageLoad} />
+                </div>
+
+                <div className={styles.hoverableDiv} onClick={togglePlay}></div>
+
+                {
+                    <div
+                        style={{
+                            position: "fixed",
+                            bottom: "10px",
+                            right: "10px",
+                            width: "330px",
+                            height: "60px",
+                            backgroundColor: "rgba(42, 42, 42, 0.8)",
+                            borderRadius: "12px",
+                            boxShadow: "0px 0px 8px rgba(0, 0, 0, 0.5)",
+                            display: "flex",
+                            alignItems: "center",
+                            justifyContent: "flex-start", // Distribute space between icons
+                            zIndex: 100,
+                        }}
+                    >
+                        <Image
+                            src="/SoundBars.png"
+                            width={33}
+                            height={33}
+                            style={{
+                                cursor: "pointer",
+                                position: "absolute",
+                                left: 20,
+                                top: "50%",
+                                transform: "translateY(-50%)",
+                            }}
+                            onLoad={handleImageLoad}
+                        />
+
+                        <div style={{ marginLeft: 68, width: 125 }}>
+                            <p style={{ fontSize: 12 }}>{songs[currentSongIndex].artist}</p>
+                            <p style={{ fontSize: 14 }}>{songs[currentSongIndex].title}</p>
+                        </div>
+
+                        <div
+                            style={{
+                                display: "flex",
+                                flex: 1,
+                                gap: 10,
+                                marginLeft: 28,
+                                alignItems: "center",
+                            }}
+                        >
+                            <Image
+                                src="/previous.png"
+                                alt="Previous"
+                                width={23}
+                                height={23}
+                                onClick={playPrevious}
+                                style={{ cursor: "pointer" }}
+                                onLoad={handleImageLoad}
+                            />
+                            <Image
+                                src={isPlaying ? "/pause.png" : "/play.png"}
+                                alt="Play/Pause"
+                                width={23}
+                                height={23}
+                                onClick={togglePlay}
+                                style={{ cursor: "pointer" }}
+                                onLoad={handleImageLoad}
+                            />
+                            <Image
+                                src="/next.png"
+                                alt="Next"
+                                width={23}
+                                height={23}
+                                onClick={playNext}
+                                style={{ cursor: "pointer" }}
+                                onLoad={handleImageLoad}
+                            />
+                        </div>
+                    </div>
+                }
+
+                <audio ref={audioRef} preload="auto"></audio>
+
+                <Benefits />
+
+                <div style={{ width: "100%", height: "100%", position: "relative" }}>
+                    {/* Background image with adjusted positioning */}
+                    <div style={{ position: "relative", zIndex: 1 }}>
+                        {/* Image positioned relative to Carousel and behind it */}
+                        <Image
+                            src="/Astronaut.png"
+                            alt="Astronaut"
+                            layout="fill"
+                            objectFit="contain"
+                            style={{
+                                position: "absolute",
+                                top: -210,
+                                left: 800,
+                                width: "100%",
+                                height: "100%",
+                                zIndex: -1,
+                            }}
+                        />
+                        <Carousel />
+                    </div>
+                    <div style={{ zIndex: 4 }}>
+                        <MemberCards />
+                        <Reroll />
+                    </div>
+                    <div className={styles.gradientBackground}>
+                        {/*<RoadmapNew/>*/}
+                        <SeasonTwo />
+                        <Team />
+                        <Advisors />
+                    </div>
+                    <FAQ />
+                    <Footer />
+                </div>
+            </main>
+        </>
+    );
 }
--- src/app/sections/Reroll.js
+++ src/app/sections/Reroll.js
@@ -1,9 +1,16 @@
-import React, { useState } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import Image from 'next/image';
 import styles from '@/app/sections/Products.module.css';
 import stylesZ from "../page.module.css";
+import useMousePosition from '../hooks/useMousePosition'; // Adjust the import path accordingly
+
 const Reroll = () => {
     const [currentStep, setCurrentStep] = useState(0); // Tracks the current active step
+    const [isCursorOverVideo, setIsCursorOverVideo] = useState(false); // Tracks if the cursor is over the video
+    const [isPointer, setIsPointer] = useState(false); // Tracks if the cursor is over an element with cursor: pointer
+    const { x, y } = useMousePosition(); // Get mouse position
+    const cursorRef = useRef(null); // Ref for the custom cursor
+    const videoRef = useRef(null); // Create a ref for the video element
 
     const steps = [
         { step: "Step 1:", title: "Mint Your Card", buttonText: "Mint" },
@@ -15,6 +22,40 @@ const Reroll = () => {
     const handleButtonClick = (stepIndex) => {
         setCurrentStep(stepIndex + 1); // Update to the next step
     };
+
+    // Effect to check if the cursor is over the video or an element with cursor: pointer
+    useEffect(() => {
+        const checkCursorOverElements = () => {
+            if (videoRef.current) {
+                const rect = videoRef.current.getBoundingClientRect();
+                setIsCursorOverVideo(x >= rect.left && x <= rect.right && y >= rect.top && y <= rect.bottom);
+            }
+
+            const elementsUnderCursor = document.elementsFromPoint(x, y);
+            const isPointerElement = elementsUnderCursor.some((el) => {
+                const computedStyle = window.getComputedStyle(el);
+                return computedStyle.cursor === 'pointer';
+            });
+
+            setIsPointer(isPointerElement);
+        };
+        checkCursorOverElements();
+    }, [x, y]);
+
+    // Effect to update the cursor position directly
+    useEffect(() => {
+        const handleMouseMove = (event) => {
+            if (cursorRef.current) {
+                cursorRef.current.style.transform = `translate3d(${event.clientX - 25}px, ${event.clientY - 25}px, 0)`;
+            }
+        };
+
+        window.addEventListener('mousemove', handleMouseMove);
+
+        return () => {
+            window.removeEventListener('mousemove', handleMouseMove);
+        };
+    }, []);
 
     return (
         <div className={styles.rerollContainer} id="reroll">
@@ -34,32 +75,30 @@ const Reroll = () => {
             </div>
 
             <div className={styles.rerollTexts}>
-                <p style={{fontSize:26}}>Got Entry Level Card?</p>
+                <p style={{ fontSize: 26 }}>Got Entry Level Card?</p>
             </div>
 
-            <div style={{margin:"0px 12px"}}>
-
-
-            <div className={styles.videoContainer}>
-                <video className={styles.videoElement} autoPlay loop muted playsInline>
-                    <source src="/reroll.mp4" type="video/mp4" />
-                    Your browser does not support the video tag.
-                </video>
-            </div>
+            <div style={{ margin: "0px 12px" }}>
+                <div className={styles.videoContainer}>
+                    <video ref={videoRef} className={styles.videoElement} autoPlay loop muted playsInline>
+                        <source src="/reroll.mp4" type="video/mp4" />
+                        Your browser does not support the video tag.
+                    </video>
+                </div>
             </div>
 
-            <p style={{fontSize: 12, color:"#e5e5e5", textAlign:"center", fontStyle:"italic", padding:"0px 10px"}}>
+            <p style={{ fontSize: 12, color: "#e5e5e5", textAlign: "center", fontStyle: "italic", padding: "0px 10px" }}>
                 $PAID Token Liquidity Pool will be Setup after FCFS Public Presale Event
             </p>
 
-            <p style={{marginTop:50, marginLeft:8, marginRight:8, textAlign:"center"}}>Swap it (NFT) for $PAID Tokens, then Re-Roll Back to NFT to get a Brand New Randomized Rarity Card</p>
+            <p style={{ marginTop: 50, marginLeft: 8, marginRight: 8, textAlign: "center" }}>Swap it (NFT) for $PAID Tokens, then Re-Roll Back to NFT to get a Brand New Randomized Rarity Card</p>
 
             {/* Step Buttons and Progress Bar at the bottom */}
             <div className={styles.rerollWrapper}>
-                <div style={{display: 'flex', justifyContent: 'space-around', margin: '20px 0', position:"sticky", zIndex:3}}>
+                <div style={{ display: 'flex', justifyContent: 'space-around', margin: '20px 0', position: "sticky", zIndex: 3 }}>
                     {steps.map((stepData, index) => (
                         <div key={index} style={{ display: "flex", flexDirection: "column", alignItems: "center" }}>
-                            <p className={styles.textP}><span style={{fontWeight:"bold", color:"#2be62c", marginRight:6}}>{stepData.step}</span>{stepData.title}</p>
+                            <p className={styles.textP}><span style={{ fontWeight: "bold", color: "#2be62c", marginRight: 6 }}>{stepData.step}</span>{stepData.title}</p>
                             <Image
                                 src={`/step${index + 1}${currentStep > index ? 'g' : ''}.png`}
                                 alt={`Step ${index + 1} Icon`}
@@ -77,14 +116,24 @@ const Reroll = () => {
                     ))}
                 </div>
 
-                <div style={{width: '100%', height: 4, backgroundColor: '#ddd', borderRadius:24}}>
-                    <div style={{width: `${(currentStep / steps.length) * 100}%`, height: '100%', backgroundColor: '#2be82b', transition: 'width 0.5s ease-in-out'}} />
+                <div style={{ width: '100%', height: 4, backgroundColor: '#ddd', borderRadius: 24 }}>
+                    <div style={{ width: `${(currentStep / steps.length) * 100}%`, height: '100%', backgroundColor: '#2be82b', transition: 'width 0.5s ease-in-out' }} />
                 </div>
             </div>
 
-            <div style={{marginTop:36, marginRight:8, marginLeft:8, textAlign:"center"}}>
-                  <p style={{fontSize:14}}>Yes, <span style={{color:"#2be82b"}}>from Entry to Emerald</span>, or Emerald back to Entry Level Member Card!</p>
+            <div style={{ marginTop: 36, marginRight: 8, marginLeft: 8, textAlign: "center" }}>
+                <p style={{ fontSize: 14 }}>Yes, <span style={{ color: "#2be82b" }}>from Entry to Emerald</span>, or Emerald back to Entry Level Member Card!</p>
             </div>
+
+            {!isPointer && (
+                <div
+                    ref={cursorRef}
+                    className={styles.customCursor}
+                    style={{ opacity: isCursorOverVideo ? 0 : 1 }}
+                >
+                    <Image src="/cursor1.png" alt="Custom Cursor" width={50} height={50} />
+                </div>
+            )}
         </div>
     );
 };
